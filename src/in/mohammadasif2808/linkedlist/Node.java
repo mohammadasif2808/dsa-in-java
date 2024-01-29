a8ff68437ffc6a6b22be0b272047d54557575318
mohammadasif2808 <mohammadasif2808@gmail.com>
2024-01-29 14:11:58 +0530

    added linked list node

---
+++ src/in/mohammadasif2808/linkedlist/Node.java
@@ -0,0 +1,42 @@
+package in.mohammadasif2808.linkedlist;
+
+/**
+ *
+ * @author mohammad asif
+ * @Email: mohammadasif2808@gmail.com
+ * @Date: Jan 29, 2024
+ * @Time: 1:54:49 PM
+ */
+/**
+ * A single generic node in the linked list.
+ *
+ * @param <T> The actual data in each node in the linked list.
+ */
+public class Node<T extends Comparable<T>> {
+
+    private T data;
+    private Node<T> next;
+
+    public Node(T data, Node<T> next) {
+        this.data = data;
+        setNext(null);
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(data);
+    }
+    
+}
