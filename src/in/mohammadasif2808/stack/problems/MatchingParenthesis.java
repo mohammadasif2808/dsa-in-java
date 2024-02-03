a8420e118213790e9a1865adf9e0dc17ddd3dcb4
mohammadasif2808 <mohammadasif2808@gmail.com>
2024-02-03 15:30:55 +0530

    working on stack parenthesis mapping problem

---
+++ src/in/mohammadasif2808/stack/problems/MatchingParenthesis.java
@@ -0,0 +1,31 @@
+package in.mohammadasif2808.stack.problems;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author mohammad asif
+ * @Email: mohammadasif2808@gmail.com
+ * @Date: Feb 3, 2024
+ * @Time: 3:24:21 PM
+ */
+public class MatchingParenthesis {
+    
+    private static final map<Character, Character> = new HashMap<>
+    
+    public static boolean hasMatchingParenthesis(String input){
+        try{
+            Stack<Character> openParenthesisStack = new Stack<>();
+            for(int i=0; i < input.length(); i++){
+                char ch = input.charAt(i);
+                if(ch=='(' or ch=='{' or ch=='['){
+                  openParenthesisStack.push(ch);  
+                }
+                
+                if()
+            }
+            
+        } catch ()
+    }
+
+}
